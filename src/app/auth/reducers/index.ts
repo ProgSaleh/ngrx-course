a0d145ccdc3eeec56d49bea837a973f27c0b90a8
ProgSaleh <progsaleh@hotmail.com>
2022-09-18 08:39:20 +0300

    full setup

---
+++ src/app/auth/reducers/index.ts
@@ -0,0 +1,14 @@
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector,
+  MetaReducer,
+} from "@ngrx/store";
+// import { environment } from "../../environments/environment";
+
+export const authFeatureKey = "auth";
+
+export interface State {}
+
+export const reducers: ActionReducerMap<State> = {};
